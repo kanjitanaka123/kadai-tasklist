e7f71d7370aeb531ba6aaf9cec839feddfe07a82
kanji.tanaka <phpkanji@gmail.com>
2020-06-02 04:44:16 +0000

    update2

--- resources/views/tasks/edit.blade.php
+++ resources/views/tasks/edit.blade.php
@@ -2,6 +2,8 @@
 
 @section('content')
 
+    @if (Auth::check())
+
     <h1>id: {{ $task->id }} のタスク編集ページ</h1>
 
     <div class="row">
@@ -21,5 +23,16 @@
             {!! Form::close() !!}
         </div>
     </div>
+    
+        @else
+        <div class="center jumbotron">
+            <div class="text-center">
+                <h1>Welcome to the tasklist</h1>
+                {{-- ユーザ登録ページへのリンク --}}
+                {!! link_to_route('signup.get', 'Sign up now!', [], ['class' => 'btn btn-lg btn-primary']) !!}
+            </div>
+        </div>
+     
+    @endif
 
 @endsection
